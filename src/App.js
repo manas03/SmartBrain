6e200e8a03f9603e4e90f49244165a94aa533393
manas03 <03.msahai03@gmail.com>
2020-03-29 01:14:21 +0530

    Adding the basic components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import React from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
 import "tachyons";
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particleOptions} />
       <Navigation />
       <Logo />
-      {/*   <ImageLinkForm />
-      <FaceRecognition /> */}
+      <Rank />
+      <ImageLinkForm />
+      {/*   <FaceRecognition /> */}
     </div>
   );
 }
